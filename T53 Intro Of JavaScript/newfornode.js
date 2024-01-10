3a717b096ffabf7b6f37f6641001b84bb2a8e5c3
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-10 17:18:59 +0530

    Intro Of JavaScript

---
+++ T53 Intro Of JavaScript/newfornode.js
@@ -0,0 +1,2 @@
+console.log("My code is running smoothly")
+console.log("My code is looking like a wow!")
---
+++ T53 Intro Of JavaScript/script.js
@@ -0,0 +1,17 @@
+alert("Hello world")
+
+console.log("My code is running smoothly")
+console.log("My code is looking like a wow!")
+
+var a = prompt("Enter any number")
+var isTrue = confirm("Your Age is 21") 
+console.log("This is my birth date " + a)
+if(isTrue){
+    console.log("You are absolutely right")
+}
+else{
+    console.log("You are wrong bro!")
+}
+
+document.title = "Beginning of JS"
+document.body.style.backgroundColor= "aqua";
