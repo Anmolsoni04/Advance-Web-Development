1fd7fa1b4c6aef88918ecb47a9c834a60f9bbe0f
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-10 20:05:34 +0530

    DataTypes & Objects in JS

---
+++ T54 Variables, DataTyp & Objects/script.js
@@ -0,0 +1,32 @@
+console.log("I'm Js Enthusiast")
+
+var a = 10;
+var b = 30;
+var c = "Anmol"
+
+console.log(a + b + 10)
+console.log(typeof a, typeof b, typeof c)
+
+// Primitive Datatypes
+
+let d = "Tanishq Soni";
+let e = true;
+let f = 120;
+let h = undefined;
+let n = null;
+
+console.log(d, e, f, h, n)
+console.log(typeof d, typeof e, typeof f, typeof h, typeof n)
+
+//objects - Non-primitive datatype
+let object = {
+    name: "Tanishq",
+    age: 20,
+    "Job Role": "Full-stack developer",
+    Job_code: 1212
+}
+console.log(object)
+
+object.location = "Mathura"
+
+console.log(object)
