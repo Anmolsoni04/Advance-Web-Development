0d5b0fb4478c6852aa5a0c55ec9debfb60bd2f1a
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-12 01:08:42 +0530

    Conditions & Operators in Js

---
+++ T55 Conditions in JS/script.js
@@ -0,0 +1,74 @@
+// console.log("So today we will stidy about conditional statements in js")
+
+// //If statements
+
+// // let age = 21;
+// // if(age > 30){
+// //     console.log("My age is 21"); 
+// // }
+// // else{
+// //     console.log("You are wrong")
+// // }
+// // //arithmetic operators:
+
+// // let marks = 3;
+// // let grace = 31;
+
+// // console.log(marks + grace)
+// // console.log(marks * grace)
+// // console.log(marks - grace)
+// // console.log(marks / grace)
+// // console.log(marks ** grace)
+// // console.log(marks == grace)
+
+// // if(marks > grace){
+// //     console.log("Your marks are " + marks ** grace)
+// // }
+
+// // else{
+// //     console.log("Your marks are" + marks * grace)
+// // }
+
+
+// let english = 30;
+// let maths = 5;
+
+// english += maths;
+// console.log(english) // 35
+
+// english =+ maths;
+// console.log(english) // 5
+
+// english = english * maths;
+// console.log(english) // 150 
+
+// //else-if ladder 
+
+
+// Basic Questions:
+
+
+// Ques:01 Use logical operators to find whether the age of a person of a person lies bw 10 & 20?
+
+// let age = 5;
+// if(age > 10 && age < 20){
+//     console.log("Age lie between 10 & 20")
+// }
+// else{
+//     console.log("Age is not lies between 10 & 20")
+// }
+
+// Ques:02 write a Js program to fimd whether the number is divisibl by 2 & 3
+
+// let number = prompt("Enter a number");
+// if (number === 0){
+//     console.log("Please enter a valid number")
+// }
+// else if(number % 2 === 0 && number % 3 === 0){
+//     console.log("This number is divisible by 2 & 3");
+// }
+// else{
+//     console.log("This number is not divisible by 2 & 3");
+// }
+
+
