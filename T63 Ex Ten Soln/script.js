f02fb4aac78375adf8a302da282a93d28cb3eb4c
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-13 00:59:15 +0530

    Ex 10 Soln

---
+++ T63 Ex Ten Soln/script.js
@@ -0,0 +1,33 @@
+// Create a Business name generator by combining lists of adjectives and shop name and other words.
+
+// Adjectives are:
+// Craze
+// Amazing
+// Fire
+
+// Shop name:
+// Engine
+// Foods
+// Garments
+
+// Other Words:
+// Bros 
+// Limited
+// Hub
+
+let adj1 = "Craze"
+let adj2 = "Amazing"
+let adj3 = "Fire"
+
+let shop1 = "Engine"
+let shop2 = "Foods"
+let shop3 = "Garments"
+
+let other1 = "Bros"
+let other2 = "Limited"
+let other3 = "Hub"
+
+console.log(adj3.concat(shop2, other3))
+// Also we can use Templating 
+
+console.log(`My Business name generator is ${adj2} ${shop2} ${other2}`)
