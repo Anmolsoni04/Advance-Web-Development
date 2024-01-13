062fdec4875ff1dbd86ef0ca0c0aae4e9f5ef847
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-14 05:19:46 +0530

    DOM Basics in JS

---
+++ T65 DOM In Js/script.js
@@ -0,0 +1,7 @@
+// DOM -> DOCUMENT OBJECT MODEL
+
+document.title='document object model'
+console.log(document.title)
+document.body.style.backgroundColor="red"
+
+document.body.style.color = "yellow"
