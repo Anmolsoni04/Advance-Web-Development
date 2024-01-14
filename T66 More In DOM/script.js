fc993fd1a5483ba0353bfe94db817c03c14bc834
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-15 01:00:07 +0530

    Parent-Child in DOM

---
+++ T66 More In DOM/script.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+// document.container.style.backgroundColor = "red"; 
+
+// document.body.childNodes
+// document.body.childNodes[0]
+// document.body.childNodes[2]
+
+// document.body.childNodes[1]
+// document.body.childNodes[1].childNodes
+
+// document.body.childNodes[3].childNodes
+
+
+// document.body.firstElementChild
+// document.body.firstElementChild.childNodes
+// console.log(document.body.firstElementChild.children)
+
+// console.log(document.body.firstElementChild.children[2])
