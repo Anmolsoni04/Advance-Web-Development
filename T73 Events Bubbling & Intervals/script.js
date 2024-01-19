15b9a4b7632113a761c7cf4ab71f57998853e652
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-19 23:11:00 +0530

    Events & Event Bubbling

---
+++ T73 Events Bubbling & Intervals/script.js
@@ -0,0 +1,19 @@
+// let btnn = document.getElementById("btn")
+
+// // Single click
+// btnn.addEventListener("click", () => {
+//     document.querySelector(".box").innerHTML = "<i>All Changes are saved</i>"
+//     alert("All changes Saved")
+// })
+
+// // Double click
+// btnn.addEventListener("dblclick", () => {
+//     document.querySelector(".box").innerHTML = "<i>All Changes are saved</i>"
+//     alert("All changes Saved")   
+// })
+
+// // for change by right click we use 'contextmenu'
+// btnn.addEventListener("contextmenu", () => {
+//     document.querySelector(".box").innerHTML = "<b>AllChangesCommits</b>"
+//     alert("Changes Done Successfully")
+// })
